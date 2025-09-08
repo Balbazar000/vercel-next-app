d2302ba8296dc3c75f9810560c336f888ea6496d
Balbazar000 <altanaltan303@gmail.com>
2025-09-08 14:19:57 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Merhaba Vercel 👋</h1>
+      <p>Bu sayfa Next.js App Router ile çalışıyor.</p>
+      <p>API denemesi: <code>/api/ping</code></p>
+    </main>
+  );
+}
